776fc636f9b965038aa6194e7d7e441a9afd8442
Phioria <chris.baugh@gmail.com>
2025-01-25 17:22:25 -0500

    Update raw-data column names

--- data-raw/foodatlas.R
+++ data-raw/foodatlas.R
@@ -21,6 +21,24 @@ if (!file.exists(lcl)) {
 
 raw_data <- readr::read_csv(file.path(lcl, atl), na = c("", "NA", "NULL"))
 
+# Keep only the columns without a significant number of NAs
 foodatlas <- raw_data[,1:25]
+
+# Rename columns for clarity and uniform lowercase
+new_colnames <- c("census_tract", "state", "county", "urban_flag", "pop2010",
+                  "ohu2010", "group_quarters_flag", "num_in_group_quarters",
+                  "pct_in_group_quarters", "li_la_1_10", "li_la_half_10",
+                  "li_la_1_20", "li_la_vehicle", "la_lva_flag",
+                  "low_income_tracts", "poverty_rate", "median_family_income",
+                  "la_1_10", "la_half_10", "la_1_20", "la_tracts_half", "la_tracts_1",
+                  "la_tracts_10", "la_tracts_20", "la_tracts_vehicle_20")
+
+colnames(foodatlas) <- new_colnames
+
+foodatlas <- foodatlas %>%
+  dplyr::mutate_at(c("urban_flag", "group_quarters_flag", "li_la_1_10", "li_la_half_10",
+                     "li_la_1_20", "li_la_vehicle", "la_lva_flag", "low_income_tracts",
+                     "la_1_10", "la_half_10", "la_1_20", "la_tracts_half", "la_tracts_1",
+                     "la_tracts_10", "la_tracts_20", "la_tracts_vehicle_20"), as.factor)
 
 usethis::use_data(foodatlas, overwrite = TRUE)
